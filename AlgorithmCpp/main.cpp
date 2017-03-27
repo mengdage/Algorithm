827b442fb13ee26e0e40aee3c0c1007dd2e1636d
meng <wulalameng@gmail.com>
2017-03-27 14:45:39 -0400

    Initial Commit

---
+++ AlgorithmCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmCpp
+//
+//  Created by LinMeng on 3/27/17.
+//  Copyright © 2017 LinMeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
